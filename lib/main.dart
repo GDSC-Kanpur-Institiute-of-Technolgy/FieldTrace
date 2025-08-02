8febf64f6358f2efe2f07e52c113cfb1eab15b5b
Manar_Elhabbal7 <manarelhabbal619@gmail.com>
2025-08-02 21:23:14 +0300

    add dark/light switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:day_night_switch/day_night_switch.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  bool isDark = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MyApp(
+      isDark: isDark,
+      onThemeChanged: (value) {
+        setState(() {
+          isDark = value;
+        });
+      },
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isDark;
+  final ValueChanged<bool> onThemeChanged;
+
+  const MyApp({
+    super.key,
+    required this.isDark,
+    required this.onThemeChanged,
+  });
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      //Theme logic here ^_^
+      
+      theme: isDark
+          ? ThemeData.dark().copyWith(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            )
+          : ThemeData.light().copyWith(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            ),
+      home: MyHomePage(
+        title: 'Flutter Demo Home Page',
+        isDark: isDark,
+        onThemeChanged: onThemeChanged,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
@@ -46,8 +70,16 @@ class MyHomePage extends StatefulWidget {
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
-
   final String title;
+  final bool isDark;
+  final ValueChanged<bool> onThemeChanged;
+
+  const MyHomePage({
+    super.key,
+    required this.title,
+    required this.isDark,
+    required this.onThemeChanged,
+  });
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -84,6 +116,12 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: [
+            DayNightSwitch(
+              value: widget.isDark,
+              onChanged: widget.onThemeChanged,
+            ),
+        ],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
